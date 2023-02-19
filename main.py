3c31a94ac27c7de5737ab14f0e838deff5f274d8
Ralfdusa <125834007+Ralfdusa@users.noreply.github.com>
2023-02-20 00:04:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
+# python3
+
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
+
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
+
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next_char in enumerate(text):
-
-        if next_char in "([{":
-            opening_brackets_stack.append(Bracket(next_char, i+1))
-
-        elif next_char in ")]}":
-            if not opening_brackets_stack:
+    for i, next in enumerate(text):
+        if next in "([{":
+            opening_brackets_stack.append(Bracket(next,i+1))
+        if next in ")]}":
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
-
-            top = opening_brackets_stack.pop()
-            if not are_matching(top.char, next_char):
-                return i+1
-
+            opening_brackets_stack.pop()
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
-
     return "Success"
 
+
 def main():
-    user_input = input("Enter the code: ")
-    mismatch = find_mismatch(user_input)
-    print(mismatch)
+    text = input()
+    mismatch = find_mismatch(text)
+    if "I" in text:
+        text = input()
+        mismatch = find_mismatch(text)
+        print(mismatch)
 
 if __name__ == "__main__":
     main()
+
